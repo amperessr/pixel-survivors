3f8723970674a3b8ec6c48f9c53b5f3cf9bca2bc
amperessr <94090494+amperessr@users.noreply.github.com>
2026-07-09 19:57:32 +0800

    Update MailData.js

--- src/mail/MailData.js
+++ src/mail/MailData.js
@@ -32,7 +32,7 @@ export const MAIL_DATA = [
     rewards: { gold: 100000, items: [] },
   },
   {
-    id: 'gold_drop_2026_07_09',
+    id: 'gold_drop2_2026_07_09',
     title: '抽獎機率補償',
     date: '2026-07-09',
     message: '各位抽起來!!',
